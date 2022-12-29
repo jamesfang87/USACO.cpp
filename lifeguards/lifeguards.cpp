bc59cb3e7b45fc8b914f060a7768c569cde42220
James Fang <113867753+jamesfang87@users.noreply.github.com>
2022-12-29 00:39:21 -0600

    Add files via upload

---
+++ lifeguards/lifeguards.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main() {
+    ifstream fin("lifeguards.in");
+    ofstream fout("lifeguards.out");
+
+    int n; fin >> n;
+
+    vector<pair<int, int>> lifeguards;
+
+    for (int i = 0; i < n; i++) {
+        pair<int, int> temp; fin >> temp.first >> temp.second;
+        lifeguards.push_back(temp);
+    }
+    
+    int covered[1000];
+    fill(covered, covered + 1000, 0);
+    
+
+    //remove each lifeguard one by one
+    int answer = 0;
+    for (pair<int, int> lifeguard : lifeguards) {
+        for (int i = lifeguard.first - 1; i < lifeguard.second - 1; i++) {
+            covered[i]++;
+        }
+    }
+    int copyCovered[1000]; 
+    copy(covered, covered + 1000, copyCovered);
+
+    for (pair<int, int> lifeguard : lifeguards) {
+        /*
+        for (int i = 0; i < 9; i++) {
+            fout << covered[i] << ' ';
+        }
+        */
+        for (int i = lifeguard.first - 1; i < lifeguard.second - 1; i++) {
+            covered[i]--;
+
+        }
+
+        
+        answer = max(1000 - (int) count(covered, covered + 1000, 0), answer);
+        //fout << '\n';
+        copy(copyCovered, copyCovered + 1000, covered);
+    }
+    fout << answer;
+    
+}
