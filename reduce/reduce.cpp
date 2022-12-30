a8a30d17c08ae87d6368213a0a701603d78a3b3b
James Fang <113867753+jamesfang87@users.noreply.github.com>
2022-12-30 01:09:16 -0600

    Add files via upload

---
+++ reduce/reduce.cpp
@@ -0,0 +1,39 @@
+// Source: https://usaco.guide/general/io
+
+#include <algorithm>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+
+	ifstream fin("reduce.in");
+	ofstream fout("reduce.out");
+
+
+	int n; fin >> n;
+	vector<int> xCo, yCo;
+
+	for (int i = 0; i < n; i++) {
+		int tempX, tempY; fin >> tempX >> tempY;
+		xCo.push_back(tempX), yCo.push_back(tempY);
+
+	}
+
+	
+
+	unsigned long long answer = 18446744073709551615ULL;
+	int answer_at = 0;
+	pair<int, int> removed;
+	pair<int, int> bruh;
+	for (int i = 0; i < n; i++) {
+		removed.first = xCo.at(i), removed.second = yCo.at(i);
+		xCo.erase(xCo.begin() + i), yCo.erase(yCo.begin() + i);
+		unsigned long long area = (*max_element(xCo.begin(), xCo.end()) - *min_element(xCo.begin(), xCo.end())) * (*max_element(yCo.begin(), yCo.end()) - *min_element(yCo.begin(), yCo.end()));
+		xCo.insert(xCo.begin() + i, removed.first);
+		yCo.insert(yCo.begin() + i, removed.second);
+		answer = min(answer, area);
+	}
+	fout << answer;
+}
