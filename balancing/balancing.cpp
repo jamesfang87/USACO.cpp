309b0a02b7d29eab376fb22ec9c800104d2f6192
James Fang <113867753+jamesfang87@users.noreply.github.com>
2023-01-09 18:25:22 -0600

    Add files via upload

---
+++ balancing/balancing.cpp
@@ -0,0 +1,53 @@
+#include <fstream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+struct cow_struct {
+    int x, y;
+};
+
+int main() {
+    ifstream fin("balancing.in");
+    ofstream fout("balancing.out");
+    int n, m; 
+    fin >> n >> m;
+
+    cow_struct cows[n];
+    set<int> xLocations, yLocations;
+
+    for (int i = 0; i < n; i++) {
+        fin >> cows[i].x >> cows[i].y;
+        xLocations.insert(cows[i].x);
+        yLocations.insert(cows[i].y);
+    }
+
+
+    int answer = n;
+    for (int xIndex = 0; xIndex < xLocations.size(); xIndex++) {
+        for (int yIndex = 0 ; yIndex < yLocations.size(); yIndex++) {
+            int quadrants[4] = {0, 0, 0, 0};
+            int xAxis = *next(xLocations.begin(), xIndex) + 1;
+            int yAxis = *next(yLocations.begin(), yIndex) + 1;
+            for (cow_struct cow: cows) {
+                if (cow.x > xAxis && cow.y > yAxis) {
+                    quadrants[0]++;
+                }  // quad1
+                else if (cow.x < xAxis && cow.y > yAxis) {
+                    quadrants[1]++;
+                }  //quad 2
+                else if (cow.x < xAxis && cow.y < yAxis) {
+                    quadrants[2]++;
+                }  //quad 3
+                else if (cow.x > xAxis && cow.y < yAxis) {
+                    quadrants[3]++;
+                }  //quad 4
+            }
+            
+            answer = min(*max_element(quadrants, quadrants + 4), answer);
+
+        }
+    }
+    fout << answer;
+
+}
